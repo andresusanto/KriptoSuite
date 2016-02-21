359d9066530f52cc277b1e1ce136f6127af128e2
Andre Susanto <andre@andresusanto.info>
2016-02-21 12:00:21 +0700

    struktur BPCS

--- src/com/andresusanto/engine/BPCS.java
+++ src/com/andresusanto/engine/BPCS.java
@@ -5,10 +5,39 @@
  */
 package com.andresusanto.engine;
 
+import java.io.IOException;
+
 /**
  *
  * @author Andre
  */
 public class BPCS {
+    // class payload data yang disimpan ke picture
+    public static class BPCSPayload{
+        public int size;
+        public String extention;
+        public byte data[];
+        
+        public BPCSPayload(int size, String extention, byte data[]){
+            this.size = size;
+            this.extention = extention;
+            this.data = data;
+        }
+    }
     
+    private Picture picture;
+    
+    public BPCS(String key, Picture picture){
+        
+    }
+    
+    // sisipkan data ke pic
+    public void embed(BPCSPayload data) throws IOException{ // throw exception jika data > payload
+        
+    }
+    
+    // ekstrak data ke pic
+    public BPCSPayload extract(){
+        return null;
+    }
 }
--- src/com/andresusanto/engine/Tools.java
+++ src/com/andresusanto/engine/Tools.java
@@ -17,4 +17,8 @@ public class Tools {
         if (value == 1) return SpacingOption.NO_SPACE;
         return SpacingOption.GROUP_5;
     }
+    
+    public static float calculatePSNR(Picture picture1, Picture picture2){
+        return 0;
+    }
 }
