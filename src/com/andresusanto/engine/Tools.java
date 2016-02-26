d694ce15054612e0c359e3e4a50802cc7ce638b0
Andre Susanto <andre@andresusanto.info>
2016-02-26 17:19:10 +0700

    Shuffle array by seed

--- src/com/andresusanto/engine/Tools.java
+++ src/com/andresusanto/engine/Tools.java
@@ -6,6 +6,10 @@
 package com.andresusanto.engine;
 
 import com.andresusanto.option.SpacingOption;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
 
 /**
  *
@@ -16,6 +20,29 @@ public class Tools {
         if (value == 0) return SpacingOption.DEFAULT;
         if (value == 1) return SpacingOption.NO_SPACE;
         return SpacingOption.GROUP_5;
+    }
+    
+    public static int[] getShuffledInts(String strSeed, int min, int max){
+        // bentuk seed untuk random
+        long seed = 0;
+        for (int i = 0; i < strSeed.length(); i++)
+            seed += (long)strSeed.charAt(i);
+        
+        // buat array berisi angka dari min sd max
+        List<Integer> arrayToShuffle = new ArrayList<Integer>();
+        for (int i = min; i <= max; i++)
+            arrayToShuffle.add(i); 
+        
+        // acak-acak dengan seed yang diberikan
+        Collections.shuffle(arrayToShuffle, new Random(seed));
+        
+        // convert ke primitive
+        int shuffledInts[] = new int[max - min + 1];
+        for (int i = 0; i < shuffledInts.length; i++){
+            shuffledInts[i] = arrayToShuffle.get(i);
+        }
+        
+        return shuffledInts;
     }
     
     public static float calculatePSNR(Picture picture1, Picture picture2){
