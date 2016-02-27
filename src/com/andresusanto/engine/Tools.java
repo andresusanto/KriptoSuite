f9b0a07d55c56bb17dc9bc093f5e00233b59021f
akhfa <akhmadfakhoni@gmail.com>
2016-02-27 23:23:48 +0700

    added byte >< int convertion

--- src/com/andresusanto/engine/Tools.java
+++ src/com/andresusanto/engine/Tools.java
@@ -111,4 +111,16 @@ public class Tools {
             System.err.print(b? 1:0);
         System.err.println("");
     }
+    
+    public static final byte[] intToByteArray(int value) {
+    return new byte[] {
+            (byte)(value >> 24),
+            (byte)(value >> 16),
+            (byte)(value >> 8),
+            (byte)value};
+    }
+    
+    public static int fromByteArray(byte[] bytes) {
+        return bytes[0] << 24 | (bytes[1] & 0xFF) << 16 | (bytes[2] & 0xFF) << 8 | (bytes[3] & 0xFF);
+    }
 }
