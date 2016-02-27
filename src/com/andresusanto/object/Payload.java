f581b4ba156caab2b008c4f9b4d472e89a74c4ab
akhfa <akhmadfakhoni@gmail.com>
2016-02-28 06:51:55 +0700

    append header + data

--- src/com/andresusanto/object/Payload.java
+++ src/com/andresusanto/object/Payload.java
@@ -19,6 +19,13 @@ import java.util.Arrays;
  */
 // class payload data yang disimpan ke picture
 public class Payload{
+    
+    // format header data yang disisipkan
+    // 1. 4 bytes (int) = ukuran file yang disisipkan
+    // 2. 1 byte = panjang nama file
+    // 3. N byte (N ditentukan oleh panjang nama file) = nama file
+    // Sehingga payload yang disisipkan : [4 bytes | 1 byte | N byte | KONTEN]
+    
     public int size;
     public String filename; // jadi filename karena ketika save as, defaultnya nama filenya adalah nama file asli
     public boolean dataAwal[]; // pesan awal yang sudah diubah dari byte ke boolean
@@ -83,13 +90,38 @@ public class Payload{
     /**
      * Men-generate array of segment yang sudah siap untuk disisipkan ke gambar.
      */
-    private void generateArrayOfSegments() //Tolong cek lagi ya. Terutama index arraycopynya
+    private void generateArrayOfSegments()
     {
         int jumlahSegmen;
-        if(this.dataAwal.length % 63 == 0)
-            jumlahSegmen = this.dataAwal.length / 63;
+        
+        // convert header jadi bit boolean array
+        // filesize dan nfilename dalam bit
+        boolean[] fileSize = Tools.convertToBoolArray(Tools.intToBytes(this.size));
+        boolean[] fileName = Tools.convertToBoolArray(Tools.stringToBytes(this.filename));
+        byte [] fileNameLengthByte = {(byte)fileName.length};
+        boolean[] nFileName = Tools.convertToBoolArray(fileNameLengthByte);
+//        System.err.println("nDataAwal = " + this.dataAwal.length);
+//        Tools.printArray(dataAwal);
+//        System.err.println("bitFileSize = " + fileSize.length);
+//        Tools.printArray(fileSize);
+//        System.err.println("bitFileName = " + fileName.length);
+//        Tools.printArray(fileName);
+//        System.err.println("nFileName = " + nFileName.length + " bit");
+//        Tools.printArray(nFileName);
+        
+        //Gabungkan header dengan data menjadi data baru
+        boolean [] headerAndData = new boolean[fileSize.length + fileName.length + 
+                                    nFileName.length + this.dataAwal.length];
+        System.arraycopy(fileSize, 0, headerAndData, 0, fileSize.length);
+        System.arraycopy(nFileName, 0, headerAndData, fileSize.length, nFileName.length);
+        System.arraycopy(fileName, 0, headerAndData, fileSize.length + nFileName.length, fileName.length);
+        System.arraycopy(this.dataAwal, 0, headerAndData, fileSize.length + nFileName.length + fileName.length, 
+                        this.dataAwal.length);
+        
+        if(headerAndData.length % 63 == 0)
+            jumlahSegmen = headerAndData.length / 63;
         else
-            jumlahSegmen = (this.dataAwal.length / 63) + 1;
+            jumlahSegmen = (headerAndData.length / 63) + 1;
         
         boolean [] dataGenap63 = new boolean[jumlahSegmen * 63]; 
         //data63 adalah data yang akan disisipkan ke pic 
@@ -102,8 +134,9 @@ public class Payload{
 //        System.err.println("init data genap 63 false: "); Tools.printArray(dataGenap63);
         
         //copy semua data ke data63
-        System.arraycopy(this.dataAwal, 0, dataGenap63, 0, dataAwal.length);
-//        System.err.println("dataAwal genap 63 false: "); Tools.printArray(dataGenap63);
+        System.arraycopy(headerAndData, 0, dataGenap63, 0, headerAndData.length);
+//        System.err.println("headerAndData genap 63 : "); Tools.printArray(dataGenap63);
+//        System.err.println("panjang headerAndData = " + dataGenap63.length); 
         
         Segments = new ArrayList<>();
         
@@ -122,5 +155,9 @@ public class Payload{
     public ArrayList<Segmen> getAllSegments()
     {
         return Segments;
+    }
+    public boolean [] getSegmenData(int index)
+    {
+        return Segments.get(index).getData();
     }
 }
