9e5fce15def396e54fd3bb91a18a6e1275182ee8
Rakhmatullah Yoga Sutrisna <rakhmatullahyoga@gmail.com>
2016-03-30 19:08:32 +0700

    merge success

--- test/com/andresusanto/test/TestECC.java
+++ test/com/andresusanto/test/TestECC.java
@@ -45,38 +45,54 @@ public class TestECC {
     }
     
     public static void tesEnkripDekrip() throws Exception{
-        Curve curve = new Curve(new BigInteger("-1"), new BigInteger("188"), new BigInteger("751"));
-        Coordinate base = new Coordinate(new BigInteger("118"), new BigInteger("29"));
-        BigInteger privateKey = new BigInteger("2213123213213412");
+        Curve curve = new Curve(new BigInteger("4"), new BigInteger("5"), new BigInteger("98764321261"));
+        Coordinate base = new Coordinate(new BigInteger("127"), new BigInteger("113"));
+        BigInteger privateKey = new BigInteger("123");
         
         ECC ecc = new ECC(curve, base, privateKey);
         
-        File file = new File("./test.bmp");
+        File file = new File("./test.png");
         byte[] fileData = new byte[(int) file.length()];
         FileInputStream in = new FileInputStream(file);
         in.read(fileData);
         in.close();
+        byte[] encryptedFile = new byte[2*fileData.length];
         byte[] decodedFile = new byte[fileData.length];
         for(int i=0; i<fileData.length; i++) {
+//            System.out.printf("asli : %2x\n", fileData[i]);
             Coordinate plain = new Coordinate(fileData[i], curve);
+//            System.out.print("Asli :\n");
+//            plain.print();
             Coordinate[] enkrip = ecc.encrypt(plain, ecc.generatePublic());
+            encryptedFile[i*2] = enkrip[0].toByte();
+            encryptedFile[i*2+1] = enkrip[1].toByte();
+//            System.out.print("Enkrip :\n");
+//            enkrip[0].print();
+//            enkrip[1].print();
+            
+            
+            
             Coordinate dekrip = ecc.decrypt(enkrip);
             decodedFile[i] = dekrip.toByte();
+//            System.out.printf("Dekrip : %2x\n", decodedFile[i]);
         }
-        FileOutputStream fw = new FileOutputStream(new File("./decoded.bmp"));
+        FileOutputStream fw = new FileOutputStream(new File("./decoded.png"));
         fw.write(decodedFile);
         fw.close();
         
-        //Coordinate plain = new Coordinate(new BigInteger("38"), new BigInteger("12"));
-        //plain.print();
-        
-        //Coordinate[] enkrip = ecc.encrypt(plain, ecc.generatePublic());
-        
+        fw = new FileOutputStream(new File("./encrypted.png"));
+        fw.write(encryptedFile);
+        fw.close();
+//        Coordinate plain = new Coordinate(new BigInteger("119"), new BigInteger("1211"));
+//        plain.print();
+//        
+//        Coordinate[] enkrip = ecc.encrypt(plain, ecc.generatePublic());
+//        
 //        for (Coordinate cor : enkrip){
 //            cor.print();
 //        }
-        
-        //Coordinate dekrip = ecc.decrypt(enkrip);
-        //dekrip.print();
+//        
+//        Coordinate dekrip = ecc.decrypt(enkrip);
+//        dekrip.print();
     }
 }
